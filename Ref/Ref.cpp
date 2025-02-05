f92e811a4414df9054dff68db554f44195ea11e8
Лютянський Дмитро Юрійович <ipz234_ldyu@student.ztu.edu.ua>
2025-02-05 22:55:17 +0200

    g

---
+++ Ref/Ref.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class User {
+public:
+    string name;
+    int age;
+
+    void printInfo() {
+        cout << "Name: " << name << ", Age: " << age << endl;
+    }
+};
+
+class Database {
+public:
+    vector<User> users;
+
+    void addUser(string name, int age) {
+        User user;
+        user.name = name;
+        user.age = age;
+        users.push_back(user);
+    }
+
+    void printAllUsers() {
+        for (int i = 0; i < users.size(); i++) {
+            cout << "User " << i + 1 << ": " << users[i].name << ", " << users[i].age << endl;
+        }
+    }
+};
+
+int main() {
+    Database db;
+    db.addUser("Alice", 25);
+    db.addUser("Bob", 30);
+    db.printAllUsers();
+    return 0;
+}
